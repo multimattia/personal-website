a0dfdfecf053be60d7afeb09e6a6f560828898cb
multimattia <58923400+multimattia@users.noreply.github.com>
2020-09-06 17:14:14 +0800

    Add prettier CSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,17 @@
 import React from "react"
 
-import Header from  "./header";
-import Footer from  "./header";
-import '../styles/index.scss';
+import Header from "./header"
+import Footer from "./footer"
+import "../styles/index.scss"
+import layoutStyles from "./layout.module.scss"
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
+    <div className={layoutStyles.container}>
+      <div className={layoutStyles.content}>
+        <Header />
         {props.children}
+      </div>
       <Footer />
     </div>
   )
